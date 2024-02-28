cdc02fb97e98e04fce903b8aada1b0913ecb41f5
takahaku <tkzhaku@gmail.com>
2024-02-28 18:00:01 +0200

    fix institution Issue

--- src/main/java/com/sintaks/mushandi/repository/InstitutionRepository.java
+++ src/main/java/com/sintaks/mushandi/repository/InstitutionRepository.java
@@ -12,4 +12,6 @@ public interface InstitutionRepository extends JpaRepository<Institution, Long> 
 	Institution checkUniqueInstitution(String institutionName,Long tuId);
 	@Query("SELECT i FROM Institution i WHERE i.institutionName = ?1 and i.id<>?2 and i.sector.tradeUnion.id = ?3")
 	Institution findByInstitutionNameAndTU(String institutionName,Long institutionId,Long tuId);
+
+    Institution findByInstitutionName(String temp);
 }
